08a7c5e76950eda1ef51966f8033ce097df44637
kaneki <pranavbhaskar23@gmai.com>
2018-02-23 04:11:10 +0530

    Semi final commit

--- app/src/main/java/com/kaneki/firebase/Details.java
+++ app/src/main/java/com/kaneki/firebase/Details.java
@@ -1,16 +1,18 @@
 package com.kaneki.firebase;
 
 public class Details {
+    public String id;
     public String med_name ;
     public String address;
-    public Double quantity;
-    public Double exp_month;
-    public Double exp_year;
+    public int quantity;
+    public int exp_month;
+    public int exp_year;
 
     public Details() {
     }
 
-    public Details(String med_name, String address, Double quantity, Double exp_month, Double exp_year) {
+    public Details(String id, String med_name, String address, int quantity, int exp_month, int exp_year) {
+        this.id = id;
         this.med_name = med_name;
         this.address = address;
         this.quantity = quantity;
--- app/src/main/java/com/kaneki/firebase/MainActivity.java
+++ app/src/main/java/com/kaneki/firebase/MainActivity.java
@@ -2,30 +2,29 @@ package com.kaneki.firebase;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
-
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
-import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    TextView d_tv_1;
     EditText d_et_1,d_et_2,d_et_3,d_et_4,d_et_5;
-    String d_s_1;
-    String d_s_2;
-    String uid;
-    double d_i_1,d_i_2,d_i_3,ud;
+    String d_s_1, d_s_2;
+    int d_i_1,d_i_2,d_i_3;
     Button d_b_1;
     Details d_d_1;
-    Random x;
     private DatabaseReference databaseReference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        d_tv_1 = (TextView) findViewById(R.id.d_tv_1) ;
 
         d_b_1 = (Button) findViewById(R.id.d_b_1);
 
@@ -35,22 +34,28 @@ public class MainActivity extends AppCompatActivity {
         d_et_4 = (EditText) findViewById(R.id.d_et_4);
         d_et_5 = (EditText) findViewById(R.id.d_et_5);
 
-        ud = x.nextDouble();
+
+
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+
+
+
 
         d_b_1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                d_s_1 = d_et_1.getText().toString();
-                d_s_2 = d_et_3.getText().toString();
-                d_i_2 = Double.parseDouble(d_et_4.getText().toString());
-                d_i_3 = Double.parseDouble(d_et_5.getText().toString());
-                d_i_1 =  Double.parseDouble(d_et_2.getText().toString());
 
-                d_d_1 = new Details(d_s_1,d_s_2,d_i_1,d_i_2,d_i_3);
+                d_s_1 = d_et_1.getText().toString().trim();
+                d_s_2 = d_et_3.getText().toString().trim();
 
-                uid = Double.toString(ud);
+                d_i_1 = Integer.parseInt( d_et_2.getText().toString() );
+                d_i_2 = Integer.parseInt(d_et_4.getText().toString().trim());
+                d_i_3 = Integer.parseInt(d_et_5.getText().toString().trim());
 
-                databaseReference.child(uid).setValue(d_d_1);
+
+                String id = databaseReference.push().getKey();
+                d_d_1 = new Details(id,d_s_1,d_s_2,d_i_1,d_i_2,d_i_3);
+                databaseReference.child(id).setValue(d_d_1);
 
                 Toast.makeText (getApplicationContext(), "Donated", Toast.LENGTH_LONG).show();
 
